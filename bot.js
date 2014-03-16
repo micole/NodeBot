8b8f960ff7b40e18c9a8215264ad5c54666667d1
micole <micole.3@gmail.com>
2014-03-16 16:12:55 -0400

    Adding really basic html grabber

--- bot.js
+++ bot.js
@@ -5,8 +5,11 @@ var config = {
     botName: "Bukkets_Baby"
 };
 
+var RegUrl = new RegExp("(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})");
+
 //lib
 var irc = require("irc");
+var http = require("follow-redirects").http;
 
 //bot
 var bot = new irc.Client(config.server, config.botName, {
@@ -25,8 +28,39 @@ bot.addListener("message", function(from, to, message) {
         sub = "https://www.google.com/search?q=" + encodeURIComponent(sub.trim());
         bot.say(to, sub);
     }
+    if(RegUrl.test(message)){
+        var url = message.match(RegUrl)[0];
+
+        http.get(url, function(result){
+            var responseParts = '';
+            result.on('data', function(chunk){
+                responseParts += chunk;
+                //bot.say(to, chunk);
+            });
+            result.on('end', function(){
+                console.log(responseParts);
+                bot.say(to, responseParts.substring(responseParts.toLowerCase().lastIndexOf("<title>")+7, responseParts.toLowerCase().lastIndexOf("</title>")));
+            });
+        });
+
+        //var req = http.request({method: 'HEAD', host: url, port: 80}, function(res) {
+        //    bot.say(to, JSON.stringify(res.headers));
+        //});
+        //req.end();
+
+        //bot.say(to, url);
+        //bot.say(to, "found URL");
+    }
 });
 
 bot.addListener('error', function(message) {
     console.log('error: ', message);
 });
+
+
+function cleanTitle(title){
+    title = title.substring(title.toLowerCase().lastIndexOf("<title>") + 7,
+                            title.toLowerCase().lastIndexOf("</title>"));
+    title = title.replace(/(\r\n|\n|\r)/gm,"");
+    title = title.replace("'", "\'");
+}
