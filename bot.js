a84865abb092e1f9886ce2fd457e2eab0ec67b73
micole <micole.3@gmail.com>
2014-03-27 22:35:48 -0400

    pushing weather and request. Need to look into the max stack size when getting a .gif request

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+//TODO: !d = definition
+//TODO: !u = urban dictionary def.
+
 //config:
 var config = {
     channels: ["#cscofc", "#micoleTest"],
@@ -5,14 +8,27 @@ var config = {
     botName: "Bukkets_Baby"
 };
 
-var RegUrl = new RegExp("(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})");
-
+//var RegUrl = new RegExp("(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})");
+var validUrl = /^(http|https):\/\/[^ "]+$/;
 //lib
 var irc = require("irc");
 var http = require("follow-redirects").http;
 var S = require("string");
 var request = require("request");
 var cheerio = require("cheerio");
+var forecast = require("forecast");
+
+//This seems to be horrible and unreliable
+var chsForecast = new forecast({
+    service: 'forcast.io',
+    key: 'your key here',
+    units: 'fahrenheit',
+    cache: true,
+    ttl: {
+        minutes: 5,
+        seconds: 0
+    }
+});
 
 //bot
 var bot = new irc.Client(config.server, config.botName, {
@@ -31,14 +47,59 @@ bot.addListener("message", function(from, to, message) {
         sub = "https://www.google.com/search?q=" + encodeURIComponent(sub.trim());
         bot.say(to, sub);
     }
-    if(RegUrl.test(message)){
-        var url = message.match(RegUrl)[0];
 
+    if(message.indexOf('!w') == 0){
+        var sub = message.split(" ");
+        if(sub[1].toLowerCase() == "charleston"){
+            chsForecast.get([32.7833, -79.9333], function(err, weather) {
+                if(err){
+                    console.log("ERROR");
+                    console.log(err);
+                } else {
+                    console.log(weather);
+                    //var jsonWeather = JSON.parse(weather);
+                    if(sub[2] == null){
+                        weatherString = "";
+                        weatherString += "Hourly: " + weather.hourly.summary; //+ " ";
+                        weatherString += " ---- Daily: " + weather.daily.summary;
+                        /*for(i = 0; i < weather.data.length; i++){
+                            //data.time given in seconds, need to convert to milliseconds
+                            date = new Date(weather.data[i].time * 1000);
+                            weatherString += "\nDATE:" + date.getMonth() + "/" + date.getDay();
+                            weatherString += " WEATHER: Min Temp:" + weather.data[i].temperatureMin;
+                            weatherString += " Max Temp:" + weather.data[i].temperatureMax;
+                        }*/
+                        bot.say(to, weatherString);
+                    }
+                    //console.log(weather);
+                }
+            });
+        } else {
+            bot.say(to, "I'm sorry, I don't have that location. Tell Micole to add that.");
+        }
+    }
+
+
+    if(validUrl.test(message)){
+        var url = message.match(validUrl)[0];
+        //bot.say(to, "Fetching: " + url);
+
+
+        //TODO: maximum stack size exceeds during imgur gif's
         request({
-            uri: url
+            uri: url,
+            timeout: 2000
         }, function(error, response, body){
-            var $ = cheerio.load(body);
-            bot.say(to, $('title').text());
+            console.log(error);
+            console.log(response);
+            if(error && error.code.toString().indexOf("ENOTFOUND") >= 0){
+                bot.say(to, url + " Doesn't seem to exist");
+            } else if(error) {
+                boy.say(to, "error");
+            } else {
+                var $ = cheerio.load(body);
+                bot.say(to, $('title').text());
+            }
         });
 
         /*try{
@@ -46,8 +107,13 @@ bot.addListener("message", function(from, to, message) {
                 var responseParts = '';
                 result.on('data', function(chunk){
                     responseParts += chunk;
+                    if(responseParts.indexOf("</title>") >= 0){
+                        result.destroy();
+                    }
                 });
                 result.on('end', function(){
+                    var $ = cheerio.load(responseParts);
+                    bot.say(to, $('title').text());
                     console.log(responseParts);
                     title = cleanTitle(responseParts);
                     if(title){
